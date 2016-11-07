6df010ba2b0998c7496a450703891385ac6fd99b
pablodecm <pablodecm@gmail.com>
2016-11-07 14:59:49 +0000

    interactive categories and scales in graph viz.

--- cartographer/visualization.py
+++ cartographer/visualization.py
@@ -1,8 +1,10 @@
 import json
 import pkg_resources
+import numpy as np
+from scipy.stats import mode
 
 
-def json_graph(model):
+def json_graph(model, categories = {}, scales = {}):
     json_dict = {}
 
     # build links json representation
@@ -18,23 +20,29 @@ def json_graph(model):
     for (p_n, p) in enumerate(model.nodes_):
         for (c_n, c) in enumerate(p):
             node_dict = {"id" : str((p_n, c_n)),
-                         "group" : 1,
+                         "int_id" : model.nodes_to_int_[(p_n, c_n)],
                          "color" : 1 } 
+            for name, arr in categories.items():
+              node_dict[name] = int(mode(arr[c], axis=None)[0])
+            for name, arr in scales.items():
+              node_dict[name] = np.mean(arr[c], axis=None)
             json_dict["nodes"].append(node_dict)
+     
+    # list of categories and scales
+    e_name = "categories_and_scales"
+    json_dict[e_name] = []
+    for c in (["int_id"] + categories.keys()):
+      json_dict[e_name].append({"name" : c, "type" : "category"})
+    for s in scales.keys():
+      json_dict[e_name].append({ "name" : s, "type" : "scale"})
 
     return json.dumps(json_dict, indent=4)
 
-def html_graph(model):
+def html_graph(model, categories = {}, scales = {}):
 
     resource_package = __name__ 
     resource_path = '/'.join(['graph_template.html'])
     template = pkg_resources.resource_string(resource_package, resource_path)
-    json_graph_str = json_graph(model)
-    template_pars = {"title" : "default title",
-                     "width" :  400,
-                     "height" : 400,
-                     "json_graph" : json_graph_str,
-                     "graph_charge" : -120,
-                     "graph_link_distance" : 30,
-                     "graph_gravity" : 0.1 }
+    json_graph_str = json_graph(model, categories, scales)
+    template_pars = { "json_graph" : json_graph_str}
     return template.format(**template_pars)
