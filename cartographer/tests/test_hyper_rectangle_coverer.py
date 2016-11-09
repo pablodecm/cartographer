15833fb4b93b03f3963993e45e60f91e56c1cddc
pablodecm <pablodecm@gmail.com>
2016-11-09 10:57:57 +0000

    add more test (exceptions and variable sized coverers)

--- cartographer/tests/test_hyper_rectangle_coverer.py
+++ cartographer/tests/test_hyper_rectangle_coverer.py
@@ -1,7 +1,7 @@
 from cartographer.coverers import HyperRectangleCoverer
 
 from sklearn.datasets.samples_generator import make_blobs
-from sklearn.utils.testing import assert_true
+from sklearn.utils.testing import assert_true, assert_raises
 import numpy as np
 
 
@@ -27,3 +27,21 @@ def test_two_dimension_int_parameters():
     assert_true(hrc.upperbounds.shape[0] == n_intervals**X.shape[1])
     m_matrix = hrc.transform(X)
     assert_true(m_matrix.shape[1] == n_intervals**X.shape[1])
+
+
+def test_three_dimension_int_wrong_cases():
+    X, true_labels = make_blobs(n_samples=1000, n_features=3)
+    intervals = [5, 10, 15]
+    overlap = [0.1, 0.2, 0.3]
+    hrc = HyperRectangleCoverer(intervals, overlap)  # right case
+    hrc_wrong_intervals = HyperRectangleCoverer([10, 5], 0.3)
+    hrc_wrong_overlap = HyperRectangleCoverer(10, [0.1, 0.2])
+    assert_raises(ValueError, hrc_wrong_intervals.fit, X)
+    assert_raises(ValueError, hrc_wrong_overlap.fit, X)
+    hrc.fit(X)
+    assert_true(hrc.lowerbounds.shape[0] == np.prod(intervals))
+    assert_true(hrc.upperbounds.shape[0] == np.prod(intervals))
+    m_matrix = hrc.transform(X)
+    assert_true(m_matrix.shape[1] == np.prod(intervals))
+    assert_raises(ValueError, hrc_wrong_overlap.fit, X, X)
+    assert_raises(ValueError, hrc_wrong_overlap.transform, X, X)
